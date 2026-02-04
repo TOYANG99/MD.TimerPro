1b80b88a5cfba6d6ae2ab9ea430105af27ba8bb5
Tommy Yang <tommyyang1999@gmail.com>
2026-02-03 18:04:22 -0600

    Inital Commit - TimerPro

---
+++ Custom/TimerLogic.cs
@@ -0,0 +1,41 @@
+namespace TimerPro.Custom;
+
+public class TimerLogic
+{
+    private int _intSec;
+    private int _intMin;
+    private int _intHour;
+
+    public TimerLogic()
+    {
+        Reset();
+    }
+
+    public void Reset()
+    {
+        _intSec = 0;
+        _intMin = 0;
+        _intHour = 0;
+    }
+
+    public void SetTickCount()
+    {
+        _intSec++;
+        if (_intSec == 60) // once timer hits 59 secs, reset secs to 0 and add a minute.
+        {
+            _intMin++;
+            _intSec = 0;
+            if (_intMin == 60) // once timer hits 59 min, reset min to 0 and add an hour.
+            {
+                _intHour++;
+                _intMin = 0;
+            }
+        }
+    }
+
+    public string GetFormatedString()
+    {
+        return _intHour.ToString().PadLeft(2, '0') + ":" +_intMin.ToString().PadLeft(2, '0') + ":" + _intSec.ToString().PadLeft(2,'0');
+    } 
+
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿namespace TimerPro;
+using TimerPro.Custom;
+public partial class MainPage : ContentPage
+{
+    private TimerLogic oTimer = new TimerLogic();
+    private bool isRunning = false;
+    
+    public MainPage()
+    {
+        InitializeComponent();
+        Title = "Timer Pro";
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        btnStart.IsEnabled = true;
+        btnStop.IsEnabled = false;
+    }
+
+    private void BtnStart_OnClicked(object sender, EventArgs e)
+    {
+        btnStart.IsEnabled = false;
+        btnStop.IsEnabled = true;
+        isRunning = true;
+        
+        Application.Current.Dispatcher.StartTimer(TimeSpan.FromSeconds(1), () =>
+        {
+            if (isRunning)
+            {
+                oTimer.SetTickCount();
+                lblDisplay.Text = oTimer.GetFormatedString();
+            }
+            return isRunning;
+        });
+    }
+
+    private void BtnStop_OnClicked(object sender, EventArgs e)
+    {
+        btnStop.IsEnabled = false;
+        btnStart.IsEnabled = true;
+        isRunning = false;
+    }
+
+    private void BtnReset_OnClicked(object sender, EventArgs e)
+    {
+        oTimer.Reset();
+        lblDisplay.Text = oTimer.GetFormatedString();
+        
+    }
+}
